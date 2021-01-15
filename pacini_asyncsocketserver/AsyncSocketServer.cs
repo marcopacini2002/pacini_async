4604168403bf417ff3e9072dc6c87c5825b9a955
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2021-01-15 12:32:17 +0100

    Add project files.

---
+++ pacini_asyncsocketserver/AsyncSocketServer.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+
+
+namespace pacini_asyncsocketserver
+{
+    public class AsyncSocketServer
+    {
+        IPAddress mIP;
+        int mPort;
+        TcpListener mServer;
+
+
+        // Server inizia as ascoltare
+        public async void InAscolto(IPAddress ipaddr = null, int port = 23000)
+        {
+            //controlli generali
+            if (ipaddr == null)
+            {
+                ipaddr = IPAddress.Any;
+            }
+            if (port < 0 || port > 65535)
+            {
+                port = 23000;
+            }
+
+            mIP = ipaddr;
+            mPort = port;
+
+            mServer = new TcpListener(mIP, mPort);
+
+            Debug.WriteLine("Server in ascolto su IP: {0} - Porta: {1}"
+                                 , mIP.ToString(), mPort.ToString());
+
+            mServer.Start();
+
+            Debug.WriteLine("Server avviato.");
+
+            TcpClient client = await mServer.AcceptTcpClientAsync();
+
+            Debug.WriteLine("Client Connesso: " + client.Client.RemoteEndPoint);
+            RiceviMessaggio(client);
+
+        }
+        public async void RiceviMessaggio(TcpClient client)
+        {
+            NetworkStream stream = null;
+            StreamReader reader = null;
+            try
+            {
+                stream = client.GetStream();
+                reader = new StreamReader(stream);
+                char[] buff = new char[512];
+                int nBytes = 0;
+                while (true)
+                {
+                    Debug.WriteLine("In attesa di un messaggio");
+                    //ricezione messaggio asincrono
+                    nBytes = await reader.ReadAsync(buff, 0, buff.Length);
+                    if (nBytes == 0)
+                    {
+                        Debug.WriteLine("Client Disconnesso");
+                        break;
+                    }
+                    string recvText = new string(buff);
+                    Debug.WriteLine("N° byte: {0}. Messaggio: {1}", nBytes, recvText);
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                Debug.WriteLine("Errore: " + ex.Message);
+            }
+
+
+        }
+    }
+}
